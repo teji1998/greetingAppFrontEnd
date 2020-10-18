2c72539d00db4c5ec20db36d121402355415c39d
“teji1998” <tejasvinirpk@gmail.com>
2020-10-19 00:25:48 +0530

    style:Added spaces in code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const addUserButton = document.getElementById("add-user-button");
+
 closeAddFormButton = document.querySelector(".close-button-for-add-user");
+
 addUserOverride = document.querySelector(".add-user-modal");
+
 addUserButton.addEventListener("click", displayAddUserForm);
+
 closeAddFormButton.addEventListener("click", closeAddUserForm);
+
 function displayAddUserForm() {
     addUserOverride.style.display = "flex";
 }
+
 function closeAddUserForm() {
     addUserOverride.style.display = "none";
 }
