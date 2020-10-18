370bc525148b3eab3504d96441405fbca4413955
“teji1998” <tejasvinirpk@gmail.com>
2020-10-19 04:06:51 +0530

    feat:Added delete user functionality

--- src/app.js
+++ src/app.js
@@ -1,32 +1,44 @@
 const addUserButton = document.getElementById("add-user-button"),
-editUserButton = document.getElementById("edit-user-button"),
-closeAddFormButton = document.querySelector(".close-button-for-add-user"),
-closeEditFormButton = document.querySelector(".close-button-for-edit-user"),
-addUserOverride = document.querySelector(".add-user-modal"),
-editUserOverride = document.querySelector(".edit-user-modal");
+  editUserButton = document.getElementById("edit-user-button"),
+  deleteUserButton = document.getElementById("delete-user-button"),
+  closeAddFormButton = document.querySelector(".close-button-for-add-user"),
+  closeEditFormButton = document.querySelector(".close-button-for-edit-user"),
+  closeDeleteFormButton = document.querySelector(".close-button-for-delete-user"),
+  addUserOverlay = document.querySelector(".add-user-modal"),
+  editUserOverlay = document.querySelector(".edit-user-modal"),
+  deleteUserOverlay = document.querySelector(".delete-user-modal");
+  
 
-
-addUserButton.addEventListener("click", displayAddUserForm);
+  addUserButton.addEventListener("click", displayAddUserForm);
 editUserButton.addEventListener("click", displayEditUserForm);
-
-
+deleteUserButton.addEventListener("click", displayDeleteUserForm);
 closeAddFormButton.addEventListener("click", closeAddUserForm);
 closeEditFormButton.addEventListener("click", closeEditUserForm);
-
+closeDeleteFormButton.addEventListener("click", closeDeleteUserForm);
 
 function displayAddUserForm() {
-    addUserOverride.style.display = "flex";
-}
+    addUserOverlay.style.display = "flex";
+  }
+  
+  function displayEditUserForm() {
+    editUserOverlay.style.display = "flex";
+  }
 
-function closeAddUserForm() {
-    addUserOverride.style.display = "none";
-}
+  function displayDeleteUserForm() {
+    deleteUserOverlay.style.display = "flex";
+  }
 
-function displayEditUserForm() {
-    editUserOverride.style.display = "flex";
-}
+  function closeAddUserForm() {
+    addUserOverlay.style.display = "none";
+  }
+    
 
-function closeEditUserForm() {
-    editUserOverride.style.display = "none";
-}
+  function closeEditUserForm() {
+    editUserOverlay.style.display = "none";
+  }
+  
+
+  function closeDeleteUserForm() {
+    deleteUserOverlay.style.display = "none";
+  }
   
