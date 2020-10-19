6f34ece156be58fa935578563316ea9239be22bf
“teji1998” <tejasvinirpk@gmail.com>
2020-10-19 08:57:50 +0530

    style:Corrected spacing and indentation

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 const addUserButton = document.getElementById("add-user-button"),
-  editUserButton = document.getElementById("edit-user-button"),
-  deleteUserButton = document.getElementById("delete-user-button"),
-  closeAddFormButton = document.querySelector(".close-button-for-add-user"),
-  closeEditFormButton = document.querySelector(".close-button-for-edit-user"),
-  closeDeleteFormButton = document.querySelector(".close-button-for-delete-user"),
-  addUserOverlay = document.querySelector(".add-user-modal"),
-  editUserOverlay = document.querySelector(".edit-user-modal"),
-  deleteUserOverlay = document.querySelector(".delete-user-modal");
+editUserButton = document.getElementById("edit-user-button"),
+deleteUserButton = document.getElementById("delete-user-button"),
+closeAddFormButton = document.querySelector(".close-button-for-add-user"),
+closeEditFormButton = document.querySelector(".close-button-for-edit-user"),
+closeDeleteFormButton = document.querySelector(".close-button-for-delete-user"),
+addUserOverride = document.querySelector(".add-user-modal"),
+editUserOverride = document.querySelector(".edit-user-modal"),
+deleteUserOverride = document.querySelector(".delete-user-modal");
   
-
-  addUserButton.addEventListener("click", displayAddUserForm);
+addUserButton.addEventListener("click", displayAddUserForm);
 editUserButton.addEventListener("click", displayEditUserForm);
 deleteUserButton.addEventListener("click", displayDeleteUserForm);
 closeAddFormButton.addEventListener("click", closeAddUserForm);
@@ -17,28 +16,26 @@ closeEditFormButton.addEventListener("click", closeEditUserForm);
 closeDeleteFormButton.addEventListener("click", closeDeleteUserForm);
 
 function displayAddUserForm() {
-    addUserOverlay.style.display = "flex";
-  }
+    addUserOverride.style.display = "flex";
+}
   
-  function displayEditUserForm() {
-    editUserOverlay.style.display = "flex";
-  }
+function displayEditUserForm() {
+    editUserOverride.style.display = "flex";
+}
 
-  function displayDeleteUserForm() {
-    deleteUserOverlay.style.display = "flex";
-  }
+function displayDeleteUserForm() {
+    deleteUserOverride.style.display = "flex";
+}
 
-  function closeAddUserForm() {
-    addUserOverlay.style.display = "none";
-  }
+function closeAddUserForm() {
+    addUserOverride.style.display = "none";
+}
     
+function closeEditUserForm() {
+    editUserOverride.style.display = "none";
+}  
 
-  function closeEditUserForm() {
-    editUserOverlay.style.display = "none";
-  }
-  
-
-  function closeDeleteUserForm() {
-    deleteUserOverlay.style.display = "none";
-  }
+function closeDeleteUserForm() {
+    deleteUserOverride.style.display = "none";
+}
   
