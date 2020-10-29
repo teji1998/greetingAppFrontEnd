bce6ccf358d22a85be97e9447117534ecfad60a4
“teji1998” <tejasvinirpk@gmail.com>
2020-10-29 17:46:26 +0530

    feat:Function to list all greetings

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const addUserButton = document.getElementById("add-user-button"),
 editUserButton = document.getElementById("edit-user-button"),
 deleteUserButton = document.getElementById("delete-user-button"),
+listAllUsersButton = document.getElementById("list-all-users-button"),
 closeAddFormButton = document.querySelector(".close-button-for-add-user"),
 closeEditFormButton = document.querySelector(".close-button-for-edit-user"),
 closeDeleteFormButton = document.querySelector(".close-button-for-delete-user"),
@@ -39,3 +40,41 @@ function closeDeleteUserForm() {
     deleteUserOverride.style.display = "none";
 }
   
+
+listAllUsersButton.addEventListener("click", getAllUsersFromDataBase);
+
+async function getAllUsersFromDataBase(e) {
+  try {
+    e.preventDefault();
+    console.log("bonjour")
+     const URL = "http://localhost:8080/greeting/all";
+
+    const response = await fetch(URL, {
+      method: 'GET',
+      headers: {
+      "Content-type": "application/json",
+      }
+    });
+
+    const results = await response.json();
+
+    let inputToHTML = ``;
+
+    await results.forEach((data) => {
+      inputToHTML += input(data);
+      document.getElementById("input-from-database").innerHTML = inputToHTML;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+
+function input(data) {
+  return `<div class="user-details-object">
+  <p class="paragraph-user-details">
+    <span id="object-id" class="user-details">Greeting id:${data.greetingId}</span>
+    <span id="greeting-message" class="user-details">Message:${data.greetingMessage} </span>
+  </p>
+  </div>`;
+}
